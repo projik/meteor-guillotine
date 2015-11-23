7fbf614c6d0d9fb172ff7c222ba1f59eb295f10e
David Baxter <davebaxter@gmail.com>
2015-11-23 11:41:36 +1100

    initial package for Metoer

---
+++ package.js
@@ -0,0 +1,17 @@
+Package.describe({
+  name: "supaseca:guillotine",
+  summary: "jQuery plugin to crop images within an area (fully responsive), allowing to drag (touch support), zoom and rotate.",
+  version: "1.3.1",
+  git: "https://github.com/projik/meteor-guillotine.git"
+});
+
+Package.onUse(function (api) {
+  api.versionsFrom('0.9.0');
+  api.use('jquery', 'client');
+  api.imply('jquery', 'client');
+  api.addFiles([
+    'guillotine/js/jquery.guillotine.js',
+    'guillotine/css/jquery.guillotine.css'
+  ], 'client'
+  );
+});
